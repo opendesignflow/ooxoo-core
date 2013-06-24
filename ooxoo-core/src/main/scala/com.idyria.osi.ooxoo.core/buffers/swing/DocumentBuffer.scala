2e27bd2658e2de3745a3c6405841c859a92a3c38
Felix Zahn <web@chatiz.de>
2013-06-24 15:10:09 +0200

    edit DocumentBuffer.scala

--- ooxoo-core/src/main/scala/com.idyria.osi.ooxoo.core/buffers/swing/DocumentBuffer.scala
+++ ooxoo-core/src/main/scala/com.idyria.osi.ooxoo.core/buffers/swing/DocumentBuffer.scala
@@ -2,17 +2,12 @@ package com.idyria.osi.ooxoo.core.buffers.swing
 
  
 import com.idyria.osi.ooxoo.core.buffers.structural._
-import scala.swing._
-import event._
-import swing._
-import swing.ListView._
 import javax.swing.text._
-import com.idyria.osi.ooxoo.core.utils.ScalaReflectUtils
-import scala.reflect.runtime.universe._
+
 
 class DocumentBuffer extends BaseBuffer {
 
-  var ucd = new PlainDocument
+  var pd = new PlainDocument
 
   
 
