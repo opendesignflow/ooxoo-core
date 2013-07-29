262d7663f739e9a4de6f94cced26415d391a17b6
rleys <leys.richard@idyria.com>
2013-07-29 13:54:19 +0200

    Added Transaction Buffer + unit Test - A Transaction Buffer retains data unit push until a thread base transaction has been commited. - TODO: Support for commit expeceptions catch and rollback

---
+++ ooxoo-core/src/main/scala/com.idyria.osi.ooxoo.core/buffers.extras.transaction/TransactionBuffer.scala
@@ -0,0 +1,230 @@
+package com.idyria.osi.ooxoo.core.buffers.extras.transaction
+
+import com.idyria.osi.ooxoo.core.buffers.structural._
+
+/**
+    This Buffer can be used to block propagations base on a transaction context,
+    so that values get propagated
+
+*/
+class TransactionBuffer extends BaseBuffer {
+
+    /**
+        The DataUnit that is currently on hold before transaction commit
+    */
+    var holdDataUnit : DataUnit = null
+
+
+    // Get
+    //----------
+
+
+    // Put
+    //------------
+
+    /**
+        Holds the dataunit for propagation until the transaction is commited
+    */
+    override def pushRight(du:DataUnit) = {
+
+        //println("In Propagate right")
+
+        this.holdDataUnit = du
+
+        Transaction() {
+
+            case Transaction.Commit(transaction) =>
+
+            //    println("Transaction Commit Called")
+
+                TransactionBuffer.this.doPushRight(holdDataUnit)
+
+            case Transaction.Cancel(transaction) =>
+
+                this.holdDataUnit = null
+
+            case _ =>
+
+        }
+
+    }
+
+    protected def doPushRight(du: DataUnit) = super.pushRight(du)
+
+}
+
+/**
+    The TransactionBuffer Singleton manages transaction state
+
+    Running Transaction Management:
+
+     - Every calling thread has a TransactionContext, in which buffers store their DataUnits to propagate
+
+*/
+object TransactionBuffer {
+
+
+
+
+}
+
+
+/**
+    A Transaction is an object holding for a transaction, the actions that should happen when commiting,
+    and also status/error informations in case of error
+*/
+class Transaction {
+
+
+    /// State of the transaction
+    var state = Transaction.State.Pending
+
+    /// The actions to be executed by the transaction
+    var actions = List[PartialFunction[Transaction,Unit]]()
+
+    /// The object that initiated this transaction, if provided at creation
+    var initiator : Any = null
+
+    // Setup
+    //------------
+
+    /**
+        The passed action closure will be executed on commit, with a reference to the current transaction passed
+    */
+    def apply( action : PartialFunction[Transaction,Unit]) = {
+
+        //println("Recording transaction action")
+        actions = action :: actions
+    }
+
+
+
+    // Lifecycle
+    //----------------
+
+    /**
+        Commits all the registered action closures
+    */
+    def commit() = {
+
+        // Change State
+        //-------------
+        this.state = Transaction.State.Commit
+
+        // Execute The actions
+        //  - Catch Errors and call roll back if needed
+        //----------------------
+        actions.foreach(_(this))
+
+    }
+
+    /**
+
+        Cancel all the actions, and discard from Transaction/Tread mapping
+
+        @return This Transaction if needs to be saved
+    */
+    def cancel() : Transaction = {
+
+        // Change State
+        //-------------
+        this.state = Transaction.State.Cancel
+
+        // Cancel
+        //----------------------
+        try {
+            actions.foreach(_(this))
+
+        }
+        // Discard
+        //--------------
+        finally {
+
+            Transaction.discard(this)
+
+        }
+        return this
+
+    }
+}
+
+object Transaction {
+
+
+    /**
+        Transaction State
+    */
+    object State extends Enumeration {
+        type State = Value
+        val Pending, Commit , Cancel, Rollback = Value
+    }
+
+    object Commit {
+
+        def unapply(transaction: Transaction) : Option[Transaction] = {
+            if (transaction.state == Transaction.State.Commit)
+                return Option(transaction)
+            else
+                return None
+        }
+    }
+
+    object Cancel {
+
+        def unapply(transaction: Transaction) : Option[Transaction] = {
+            if (transaction.state == Transaction.State.Cancel)
+                return Option(transaction)
+            else
+                return None
+        }
+    }
+
+
+
+
+    var currentTransactions = Map[Thread,Transaction]()
+
+
+
+    implicit var defaultInitiator : Any = null
+
+    /**
+
+        @return The Transaction of the calling Buffer for the current Thread
+    */
+    def apply( implicit initiator : Any) : Transaction = {
+
+        // Take current Thread
+        //-------------
+        var thread = Thread.currentThread
+
+        // Create or return Transaction
+        //---------------
+        currentTransactions.get(thread) match {
+
+            case Some(transaction) => transaction
+            case None =>
+
+                    var transaction = new Transaction
+                    transaction.initiator = initiator
+                    currentTransactions+= (thread -> transaction)
+                    return transaction
+        }
+
+    }
+
+
+    /**
+        Remove a transaction from Thread Mapping
+    */
+    def discard(transaction : Transaction) = {
+
+        currentTransactions.find{case (th , tr) => tr==transaction } match {
+            case Some((th,tr)) => currentTransactions -= th
+            case None =>
+                // FIXME : Return an error trying to discard a non registered transaction ?
+        }
+
+    }
+
+}
---
+++ ooxoo-core/src/test/scala/com.idyria.osi.ooxoo.core/buffers.extras.transaction/TransactionBufferTest.scala
@@ -0,0 +1,177 @@
+package com.idyria.osi.ooxoo.core.buffers.extras.transaction
+
+
+
+import org.scalatest._
+import org.scalatest.matchers._
+
+import com.idyria.osi.ooxoo.core.buffers.extras.transaction._
+import com.idyria.osi.ooxoo.core.buffers.structural._
+import com.idyria.osi.ooxoo.core.buffers.datatypes._
+
+import com.idyria.osi.tea.listeners._
+
+
+class TransactionBufferTest extends FeatureSpec with GivenWhenThen  with ShouldMatchers {
+
+    /**
+        Buffer designed to catch transaction propagations and report what it saw
+    */
+    class ReceiveTransactionBuffer extends BaseBuffer  with ListeningSupport {
+
+
+
+
+    }
+
+
+    feature("Transaction Begin") {
+
+         scenario("Two Begins in one thread give one transaction") {
+
+            var transaction1 = Transaction()
+            var transaction2 = Transaction()
+
+            assert(transaction1.hashCode == transaction2.hashCode)
+
+         }
+
+         scenario("Two Begins in one thread give Two threads give two transactions") {
+
+            Given ("A Transaction from one thread")
+
+            var transaction1 : Transaction = null
+            var th1 = new Thread() {
+                override def run() = {
+                    transaction1 = Transaction()
+                }
+            }
+
+            And("A transaction fron another thread")
+
+            var transaction2 : Transaction = null
+            var th2 = new Thread() {
+                override def run() = {
+                    transaction2 = Transaction()
+                }
+            }
+
+
+            Then("The two transactions are different")
+            th1.start();
+            th2.start();
+            List(th1,th2).foreach(_.join)
+
+            assert(transaction1.hashCode != transaction2.hashCode)
+
+         }
+
+        scenario(" Transaction Data Unit Holding") {
+
+            Given("A Transactional Buffer chain")
+            //--------------------
+
+            var baseBuffer = new LongBuffer()
+            baseBuffer - new TransactionBuffer()
+
+            When("Setting Value on base long buffer")
+            //------------
+            baseBuffer.set(42)
+
+            Then(" there is transaction in TransactionBuffer, with one registered partitial function")
+            //---------------
+            var transaction = Transaction()
+            assert(transaction!=null,"Transaction for current Thread must not be null")
+
+            transaction.actions.size should equal (1)
+
+        }
+
+    }
+
+    feature("Transaction Commit") {
+
+
+        scenario("Single Value set commit ") {
+
+            Given("A Transactional Buffer chain")
+            //--------------------
+            var resultDataUnit : DataUnit = null
+            var baseBuffer = new LongBuffer()
+            baseBuffer - new TransactionBuffer()
+            baseBuffer - new BaseBuffer() {
+
+                override def pushRight(du:DataUnit) = {
+                    resultDataUnit = du
+                }
+
+            }
+
+            When("Setting Value on base start buffer, ")
+            //------------------------------
+            baseBuffer.set(42)
+
+            Then("the receiving buffer should't get any results")
+            //-----------------------------
+            expectResult(null)(resultDataUnit)
+
+            When("commiting the transaction")
+            //---------------------------------
+            Transaction().commit
+
+            Then("The holded data unit gets received by the buffer after transaction buffer")
+            //---------------------
+            assert(resultDataUnit!=null)
+
+
+        }
+
+        scenario("Multiple Buffer commit") (pending)
+
+
+    }
+
+    feature("Transaction Cancel") {
+
+        scenario("Single Cancel") {
+
+            Given("A Transactional Buffer chain")
+            //--------------------
+            var resultDataUnit : DataUnit = null
+            var baseBuffer = new LongBuffer()
+            baseBuffer - new TransactionBuffer()
+            baseBuffer - new BaseBuffer() {
+
+                override def pushRight(du:DataUnit) = {
+                    resultDataUnit = du
+                }
+
+            }
+
+            When("Setting Value on base start buffer, ")
+            //------------------------------
+            baseBuffer.set(42)
+
+
+            And("Cancelling the transaction")
+            //---------------------------------
+            var transaction = Transaction()
+            Transaction().cancel
+
+            Then("No result should be received")
+            //-----------------------
+            assert(resultDataUnit==null)
+
+            And("Transaction has been discarded")
+            //----------------
+            var transactionAfterCancel = Transaction()
+            assert(transaction.hashCode != transactionAfterCancel.hashCode)
+            expect(Transaction.State.Pending)(transactionAfterCancel.state)
+
+
+        }
+    }
+
+
+}
+
