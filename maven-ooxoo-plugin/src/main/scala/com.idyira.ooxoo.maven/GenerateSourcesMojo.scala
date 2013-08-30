6b416e36d60de588b198f9b735d97d3400e03e37
rleys <richard.leys@ziti.uni-heidelberg.de>
2013-08-30 17:01:54 +0200

    FIXED: Previous commit was omitting to add outputs to compilation unit

--- maven-ooxoo-plugin/src/main/scala/com.idyira.ooxoo.maven/GenerateSourcesMojo.scala
+++ maven-ooxoo-plugin/src/main/scala/com.idyira.ooxoo.maven/GenerateSourcesMojo.scala
@@ -39,131 +39,139 @@ class GenerateSourcesMojo extends AbstractMojo with MavenReport  {
 
         // Checks
         //---------------
-        if (!modelsFolder.exists)
-            return
+        if (modelsFolder.exists) {
 
-        //-- Parameters
-        //-------------------
+            //-- Parameters
+            //-------------------
 
-        //-- Map to store instances of producers, for reuse purpose
-        var producers = Map[Class[ _ <: Producer],Producer]()
+            //-- Map to store instances of producers, for reuse purpose
+            var producers = Map[Class[ _ <: Producer],Producer]()
 
-        // Variables for compiler
-        //------------------
+            // Variables for compiler
+            //------------------
 
-        //-- Version
-        /*ModelCompiler.bind("groupId",project.getGroupId)
-        ModelCompiler.bind("artifactId",project.getArtifactId)
-        ModelCompiler.bind("version",project.getVersion)*/
- 
-        //-- Search the xmodels
-        //---------------------------------
-        var xmodelsFiles = modelsFolder.listFiles(new FilenameFilter() {
+            //-- Version
+            /*ModelCompiler.bind("groupId",project.getGroupId)
+            ModelCompiler.bind("artifactId",project.getArtifactId)
+            ModelCompiler.bind("version",project.getVersion)*/
+     
+            //-- Search the xmodels
+            //---------------------------------
+            var xmodelsFiles = modelsFolder.listFiles(new FilenameFilter() {
 
-            def accept(dir:File,name:String) : Boolean = {
-                name.matches(".*\\.xmodel")
-            } 
-
-        })
-
-        //-- Process all models
-        //--  - First Filter the on that don't have to be regenerated
-        //--  - Then Produce
-        //------------------
-        xmodelsFiles.filter {
-            f =>
-                // Get or set a timestamp file to detect if model file changed since last run
-                //-------------------
-
-                //-- Set timestamps. If modified is bigger than last timestamp -> regenerate
-                var lastTimeStamp : Long = 0
-                var lastModified = f.lastModified
-
-                var statusFolder = new File("target/maven-status/maven-ooxoo-plugin")
-                statusFolder.mkdirs
-                var timestampFile = new File(statusFolder,s"${f.getName}.ts")
-                timestampFile.exists match {
-                    case true => 
-                        lastTimeStamp = java.lang.Long.parseLong(Source.fromFile(timestampFile).mkString)
-                    case false =>
-                }
- 
-                // Write Actual timestamp
-                //-------------
-                java.nio.file.Files.write(timestampFile.toPath,new String(s"${System.currentTimeMillis}").getBytes)
-
-                lastModified > lastTimeStamp
-
-        }.foreach {
-            f => 
-                getLog().info( "(Re)generating model: "+f );
-
-                // Get Model as String
-                //--------------------------
-                var source = Source.fromFile(f)
-                var content = source.mkString
-
-                
-                
-
-                // Compile to get annotated producers
-                //---------------------
-                var modelInfos = ModelCompiler.compile(f)
-
-                // Produce for all defined producers
-                //---------------
-                if (modelInfos.producers!=null && modelInfos.producers.value()!=null) {
-                    modelInfos.producers.value().foreach {
-                        producerAnnotation => 
-
-                            // Get Producer
-                            //---------
-                            var producer = producers.get(producerAnnotation.value) match {
-                                    case Some(producer) => 
-                                        producer 
-                                    case None =>  
-                                        var producer = producerAnnotation.value.newInstance
-                                        producers = producers +  (producerAnnotation.value -> producer)
-                                        producer
-                            }               
-
-                            // Produce or produce later
-                            //----------
-                            producer.outputType match {
-                                
-                                // Report, so save and generate when reports are generated
-                                case outputType if(outputType.startsWith("report.")) =>
-
-
-
-                                // Produce now as sources
-                                case _ => 
-
-                                    // Prepare Output
-                                    //--------------
-                                    var outputFolder = new File(outputBaseFolder,producer.outputType)
-                                    outputFolder.mkdirs()
-                                    var out = new FileWriters(outputFolder)
-
-                                    ModelCompiler.produce(modelInfos,producer,out)
-
-                                    // Add Target Folder to compile source if existing
-                                    //-----------------
-                                    if (outputFolder.exists) {
-                                        this.project.addCompileSourceRoot(outputFolder.getAbsolutePath);
-                                    }
-                            }
-                            
-
-                    }
-                    // EOF Foreach producers
+                def accept(dir:File,name:String) : Boolean = {
+                    name.matches(".*\\.xmodel")
                 } 
+
+            })
+
+            //-- Process all models
+            //--  - First Filter the on that don't have to be regenerated
+            //--  - Then Produce
+            //------------------
+            xmodelsFiles.filter {
+                f =>
+                    // Get or set a timestamp file to detect if model file changed since last run
+                    //-------------------
+
+                    //-- Set timestamps. If modified is bigger than last timestamp -> regenerate
+                    var lastTimeStamp : Long = 0
+                    var lastModified = f.lastModified
+
+                    var statusFolder = new File("target/maven-status/maven-ooxoo-plugin")
+                    statusFolder.mkdirs
+                    var timestampFile = new File(statusFolder,s"${f.getName}.ts")
+                    timestampFile.exists match {
+                        case true => 
+                            lastTimeStamp = java.lang.Long.parseLong(Source.fromFile(timestampFile).mkString)
+                        case false =>
+                    }
+     
+                    // Write Actual timestamp
+                    //-------------
+                    java.nio.file.Files.write(timestampFile.toPath,new String(s"${System.currentTimeMillis}").getBytes)
+
+                    lastModified > lastTimeStamp
+
+            }.foreach {
+                f => 
+                    getLog().info( "(Re)generating model: "+f );
+
+                    // Get Model as String
+                    //--------------------------
+                    var source = Source.fromFile(f)
+                    var content = source.mkString
+
+                    
+                    
+
+                    // Compile to get annotated producers
+                    //---------------------
+                    var modelInfos = ModelCompiler.compile(f)
+
+                    // Produce for all defined producers
+                    //---------------
+                    if (modelInfos.producers!=null && modelInfos.producers.value()!=null) {
+                        modelInfos.producers.value().foreach {
+                            producerAnnotation => 
+
+                                // Get Producer
+                                //---------
+                                var producer = producers.get(producerAnnotation.value) match {
+                                        case Some(producer) => 
+                                            producer 
+                                        case None =>  
+                                            var producer = producerAnnotation.value.newInstance
+                                            producers = producers +  (producerAnnotation.value -> producer)
+                                            producer
+                                }               
+
+                                // Produce or produce later
+                                //----------
+                                producer.outputType match {
+                                    
+                                    // Report, so save and generate when reports are generated
+                                    case outputType if(outputType.startsWith("report.")) =>
+
+
+
+                                    // Produce now as sources
+                                    case _ => 
+
+                                        // Prepare Output
+                                        //--------------
+                                        var outputFolder = new File(outputBaseFolder,producer.outputType)
+                                        outputFolder.mkdirs()
+                                        var out = new FileWriters(outputFolder)
+
+                                        ModelCompiler.produce(modelInfos,producer,out)
+
+                                        // Add Target Folder to compile source if existing
+                                        //-----------------
+                                        if (outputFolder.exists) {
+                                            this.project.addCompileSourceRoot(outputFolder.getAbsolutePath);
+                                        }
+                                }
+                                
+
+                        }
+                        // EOF Foreach producers
+                    } 
+            }
+            // EOF Xfiles loop
+
+
+
         }
-        // EOF Xfiles loop
+
+        
 
         // Add All Target Folder generated sources as compile unit
         //-------------------
-        outputBaseFolder.listFiles.filter(_.isDirectory).foreach(f => this.project.addCompileSourceRoot(f.getAbsolutePath))
+        if (outputBaseFolder.exists) {
+            outputBaseFolder.listFiles.filter(_.isDirectory).foreach(f => this.project.addCompileSourceRoot(f.getAbsolutePath))
+        }
+        
          
 
     }
