9437a626179903a2886852880c7f2f7a74801fd1
rleys <richard.leys@ziti.uni-heidelberg.de>
2013-11-22 14:12:37 +0100

    Added Support of enumerations to Attributes

--- ooxoo-core/src/main/scala/com.idyria.osi.ooxoo.model/ModelBuilderLanguage.scala
+++ ooxoo-core/src/main/scala/com.idyria.osi.ooxoo.model/ModelBuilderLanguage.scala
@@ -6,311 +6,333 @@ import com.idyria.osi.ooxoo.core.buffers.datatypes._
 import com.idyria.osi.ooxoo.core.buffers.structural._
 import com.idyria.osi.tea.listeners.ListeningSupport
 
-
 /**
-    This trait contains all the language Wrappers and Conversions used in Model Builder.
-    It is separated to lighten real model management in ModelBuilder from convienience language
-*/
+ * This trait contains all the language Wrappers and Conversions used in Model Builder.
+ * It is separated to lighten real model management in ModelBuilder from convienience language
+ */
 trait ModelBuilderLanguage extends ListeningSupport {
 
-    // Typing
-    //------------------------------
+  // Typing
+  //------------------------------
+
+  /**
+   * Types map is used to map type strings to actual Buffer implementation
+   */
+  var typesMap = Map[String, Class[_ <: Buffer]](
+    ("string" -> classOf[XSDStringBuffer]),
+    ("cdata" -> classOf[CDataBuffer]),
+    ("int" -> classOf[IntegerBuffer]),
+    ("integer" -> classOf[IntegerBuffer]),
+    ("long" -> classOf[LongBuffer]),
+    ("float" -> classOf[FloatBuffer]),
+    ("datetime" -> classOf[DateTimeBuffer]),
+    ("boolean" -> classOf[BooleanBuffer]),
+    ("bool" -> classOf[BooleanBuffer]),
+    ("regexp" -> classOf[RegexpBuffer]),
+    ("uri" -> classOf[URIBuffer]),
+
+    ("map" -> classOf[StringMapBuffer])
+  )
+
+  def getType(str: String): Class[_ <: Buffer] = {
+
+    this.typesMap.get(str) match {
+      case Some(resClass) => resClass
+      case None           => throw new RuntimeException(s"""ModelBuilderLanguage: Specified type "$str" did not map to a registered Class""")
+    }
+  }
+
+  //-- Init
+
+  // Element Language
+  //-----------------------
+
+  class IsWordElementWrapper(var left: Element) {
+
+    def multiple(typeStr: String) = {
+
+      @->("element.start", left)
+
+      left.classType = getType(typeStr.toLowerCase()).getCanonicalName()
+      left.maxOccurs = 10
+
+      @->("element.end", left)
+
+    }
+
+    def multiple(right: => Any): Element = {
+
+      @->("element.start", left)
+
+      left.maxOccurs = 10
+
+      right
+
+      @->("element.end", left)
+
+      left
+
+    }
+
+    def multipleOf(right: Element): IsWordElementWrapper = {
+
+      @->("element.start", left)
+
+      left.maxOccurs = 10
+
+      left.classType = right.name
+      left.instanceOfElement = right
+
+      @->("element.end", left)
+
+      this
+
+    }
 
     /**
-        Types map is used to map type strings to actual Buffer implementation
-    */
-    var typesMap = Map[String,Class[_ <: Buffer]](
-        ("string" ->  classOf[XSDStringBuffer]),
-        ("cdata" ->  classOf[CDataBuffer]),
-        ("int" ->  classOf[IntegerBuffer]),
-        ("integer" ->  classOf[IntegerBuffer]),
-        ("long" ->  classOf[LongBuffer]),
-        ("float" ->  classOf[FloatBuffer]),
-        ("datetime" ->  classOf[DateTimeBuffer]),
-        ("boolean" ->  classOf[BooleanBuffer]),
-        ("bool" ->  classOf[BooleanBuffer]),
-        ("regexp" ->  classOf[RegexpBuffer]),
-        ("uri" -> classOf[URIBuffer]),
-        
-        ("map" -> classOf[StringMapBuffer])
-    )
+     * The right string is used either:
+     *
+     * - As Type information if the type has not been set
+     * - A Description if the type has already been set.
+     *
+     * This allows following syntax:
+     *
+     * "Element" ofType("string") is ("An element contanining certain informations")
+     *
+     */
+    def is(right: String): IsWordElementWrapper = {
 
-    def getType(str: String) : Class[_ <: Buffer] = {
+      left.classType match {
 
-        this.typesMap.get(str) match {
-            case Some(resClass) => resClass
-            case None => throw new RuntimeException(s"Specificed type $str did not map to a registered Class")
-        }
-    }
+        // Type
+        //--------------
+        case null =>
 
-    //-- Init
-  
+          @->("element.start", left)
 
-    // Element Language
-    //-----------------------
-
-    class IsWordElementWrapper( var left: Element) {
-
+	      left.classType = getType(right.toLowerCase()).getCanonicalName
+	
+	      @->("element.end", left)
+          
+        // Description
+        //-------------------
+        case _ => 
+          left.description = right
+      }
 
       
-        def multiple(typeStr: String) = {
 
-            @->("element.start",left)
+      this
 
-            left.classType = getType(typeStr.toLowerCase()).getCanonicalName()
-            left.maxOccurs = 10
-
-            @->("element.end",left)
-
-        }
-
-        def multiple(right: => Any) : Element = {
-
-            @->("element.start",left)
-
-            left.maxOccurs = 10
-
-            right
-
-            @->("element.end",left)
-
-            left 
-
-        }
-
-        def multipleOf(right: Element ) : IsWordElementWrapper = {
-
-            @->("element.start",left)
-
-            left.maxOccurs = 10
-
-            left.classType = right.name
-            left.instanceOfElement = right
-
-            @->("element.end",left)
-
-            this
-
-        }
-
-        //implicit val defaultDesc = { "" } 
-        def is(right: String): IsWordElementWrapper = {
-
-            // Description
-            //left.description = desc
-        
-
-            @->("element.start",left)
-
-            left.classType = getType(right.toLowerCase()).getCanonicalName
-
-            @->("element.end",left)
-
-            this
-
-        }
-
-         /**
-            Set type of element based on string
-        */
-        def ofType( right : String) : IsWordElementWrapper = {
-
-            @->("element.start",left)
-
-            left.classType = getType(right.toLowerCase()).getCanonicalName
-
-            @->("element.end",left)
-
-            this
-
-        }
-        
-        /**
-         * Set Enumeration type
-         */
-        def enum(values: String*) : IsWordElementWrapper = {
-          
-          @->("element.start",left)
-
-            left.classType = classOf[EnumerationBuffer].getCanonicalName
-            values.foreach(left.enumerationValues+=_)
-            @->("element.end",left)
-          
-          this
-          
-        }
-        
-
-        def and : IsWordElementWrapper = this
-        
-        def and(str: String) : IsWordElementWrapper = {
-
-            left.description = str
-            this 
-        }
-
- 
-        def is(right: => Any) : Element = {
-
-            //println("in is definition for Element")
-
-            @->("element.start",left)
-
-            right
-
-            @->("element.end",left)
-
-            left
-        }
-
-       
-
-        def as ( right: Class[_ <: Buffer]) : Element = {
-
-            @->("element.start",left)
-
-            left.classType = right.getCanonicalName
-
-            @->("element.end",left)
-
-            left 
-        }
-
-        def as(right: String): Element = {
-
-            @->("element.start",left)
-
-            left.classType = getType(right.toLowerCase()).getCanonicalName
-
-            @->("element.end",left)
-
-            left
-
-        }
-        
-        // Documentation
-        //------------------
-        
-        /**
-         * Alias to withDocumentation
-         */
-        def means(str:String) = withDocumentation(str)
-        
-        /**
-         * Set documentation on element
-         */
-        def withDocumentation(str:String) : IsWordElementWrapper = {
-          
-          left.description = str
-          this
-        }
-        
-    }
-    implicit def elementToIsWordWrapping(str: String) :  IsWordElementWrapper = new IsWordElementWrapper(str)
-
-   
-    // Attribute Language
-    //------------------------------
-    class IsWordAttributeWrapper( var left: Attribute) {
-
-        def attribute(right: => Unit) = is(right)
-        def attribute( right : String) = is(right)
-        def attribute( right: Class[_ <: Buffer]) = is(right)
-
-        def is(right: => Unit) = {
-
-            @->("attribute.add",left)
-
-            right
-
-        }
-
-       
-
-        /**
-            Set type of attribute based on string
-        */
-        def ofType( right : String) : IsWordAttributeWrapper = {
-
-            @->("attribute.add",left)
-
-            // Search for type in internal map
-            //--------------------------
-            left.classType = getType(right.toLowerCase()).getCanonicalName
-
-            this
-
-        }
-
-        def is ( right: Class[_ <: Buffer]) = {
-
-            @->("attribute.add",left)
-            left.classType = right.getCanonicalName
-        }
-
-        /**
-            Description
-        */
-        def and(str: String) : IsWordAttributeWrapper = {
-
-            left.description = str
-            this 
-        }
-        
-        /**
-            Description
-        */
-        def means(str: String) : IsWordAttributeWrapper = {
-
-            left.description = str
-            this 
-        }
-        
-         /**
-            Description
-        */
-        def is( right : String) : IsWordAttributeWrapper = {
-
-            left.description = right
-            this 
-
-        }
     }
 
-    class AttributeWordAttributeWrapper( var left: Attribute) {
+    /**
+     * Set type of element based on string
+     */
+    def ofType(right: String): IsWordElementWrapper = {
 
+      @->("element.start", left)
 
-        def attribute(right: => Unit) = {
+      left.classType = getType(right.toLowerCase()).getCanonicalName
 
-            @->("attribute.add",left)
+      @->("element.end", left)
 
-            right
+      this
 
-        }
-
-        /**
-            Set type of attribute based on string
-        */
-        def attribute( right : String) = {
-
-            @->("attribute.add",left)
-
-            // Search for type in internal map
-            //--------------------------
-            left.classType = getType(right.toLowerCase()).getCanonicalName
-
-        }
-
-        def attribute ( right: Class[_ <: Buffer]) = {
-
-            @->("attribute.add",left)
-
-            left.classType = right.getCanonicalName
-
-        }
     }
 
+    /**
+     * Set Enumeration type
+     */
+    def enum(values: String*): IsWordElementWrapper = {
 
-    implicit def attributeToIsWordWrapping(attr: Attribute) :  IsWordAttributeWrapper = new IsWordAttributeWrapper(attr)
-    implicit def attributeStrToAttributeWordWrapping(attr: String) :  AttributeWordAttributeWrapper = new AttributeWordAttributeWrapper(attr)
+      @->("element.start", left)
+
+      left.classType = classOf[EnumerationBuffer].getCanonicalName
+      values.foreach(left.enumerationValues += _)
+      @->("element.end", left)
+
+      this
+
+    }
+
+    def and: IsWordElementWrapper = this
+
+    def and(str: String): IsWordElementWrapper = {
+
+      left.description = str
+      this
+    }
+
+    def is(right: => Any): Element = {
+
+      //println("in is definition for Element")
+
+      @->("element.start", left)
+
+      right
+
+      @->("element.end", left)
+
+      left
+    }
+
+    def as(right: Class[_ <: Buffer]): Element = {
+
+      @->("element.start", left)
+
+      left.classType = right.getCanonicalName
+
+      @->("element.end", left)
+
+      left
+    }
+
+    def as(right: String): Element = {
+
+      @->("element.start", left)
+
+      left.classType = getType(right.toLowerCase()).getCanonicalName
+
+      @->("element.end", left)
+
+      left
+
+    }
+
+    // Documentation
+    //------------------
+
+    /**
+     * Alias to withDocumentation
+     */
+    def means(str: String) = withDocumentation(str)
+
+    /**
+     * Set documentation on element
+     */
+    def withDocumentation(str: String): IsWordElementWrapper = {
+
+      left.description = str
+      this
+    }
+
+  }
+  implicit def elementToIsWordWrapping(str: String): IsWordElementWrapper = new IsWordElementWrapper(str)
+
+  // Attribute Language
+  //------------------------------
+  class IsWordAttributeWrapper(var left: Attribute) {
+
+    def attribute(right: => Unit) = is(right)
+    def attribute(right: String) = is(right)
+    def attribute(right: Class[_ <: Buffer]) = is(right)
+
+    def is(right: => Unit) = {
+
+      @->("attribute.add", left)
+
+      right
+
+    }
+
+    /**
+     * Set type of attribute based on string
+     */
+    def ofType(right: String): IsWordAttributeWrapper = {
+
+      @->("attribute.add", left)
+
+      // Search for type in internal map
+      //--------------------------
+      left.classType = getType(right.toLowerCase()).getCanonicalName
+
+      this
+
+    }
+    
+    /**
+     * Set Enumeration type
+     */
+    def enum(values: String*): IsWordAttributeWrapper = {
+
+      @->("attribute.add", left)
+
+      left.classType = classOf[EnumerationBuffer].getCanonicalName
+      values.foreach(left.enumerationValues += _)
+      
+
+      this
+
+    }
+
+    def is(right: Class[_ <: Buffer]) = {
+
+      @->("attribute.add", left)
+      left.classType = right.getCanonicalName
+    }
+
+    /**
+     * Description
+     */
+    def and(str: String): IsWordAttributeWrapper = {
+
+      left.description = str
+      this
+    }
+
+    /**
+     * Description
+     */
+    def means(str: String): IsWordAttributeWrapper = {
+
+      left.description = str
+      this
+    }
+
+    /**
+     * Description
+     */
+    def is(right: String): IsWordAttributeWrapper = {
+
+      left.description = right
+      this
+
+    }
+  }
+
+  class AttributeWordAttributeWrapper(var left: Attribute) {
+
+    def attribute(right: => Unit) = {
+
+      @->("attribute.add", left)
+
+      right
+
+    }
+
+    /**
+     * Set type of attribute based on string
+     */
+    def attribute(right: String) = {
+
+      @->("attribute.add", left)
+
+      // Search for type in internal map
+      //--------------------------
+      left.classType = getType(right.toLowerCase()).getCanonicalName
+
+    }
+
+    def attribute(right: Class[_ <: Buffer]) = {
+
+      @->("attribute.add", left)
+
+      left.classType = right.getCanonicalName
+
+    }
+  }
+
+  implicit def attributeToIsWordWrapping(attr: Attribute): IsWordAttributeWrapper = new IsWordAttributeWrapper(attr)
+  implicit def attributeStrToAttributeWordWrapping(attr: String): AttributeWordAttributeWrapper = new AttributeWordAttributeWrapper(attr)
 }
--- ooxoo-core/src/main/scala/com.idyria.osi.ooxoo.model/ModelCompiler.scala
+++ ooxoo-core/src/main/scala/com.idyria.osi.ooxoo.model/ModelCompiler.scala
@@ -1,30 +1,24 @@
 package com.idyria.osi.ooxoo.model
 
 import java.io._
-
 import scala.io._
 import scala.tools.nsc._
 import scala.tools.nsc.interpreter._
-
 import scala.runtime._
-
 import java.net._
-
 import scala.collection.JavaConversions._
 
 /**
-    Return informations about a model
-*/
+ * Return informations about a model
+ */
 class ModelInfos(
 
-    var name : String
+    var name: String) {
 
-    ) {
-
-    /**
-        The list of @producer annotation instances declared on model
-    */
-    var producers : producers = null
+  /**
+   * The list of @producer annotation instances declared on model
+   */
+  var producers: producers = null
 
 }
 
@@ -34,172 +28,185 @@ class ModelInfos(
 */
 object ModelCompiler {
 
-    // Run Statistics
-    //---------------------
+  // Run Statistics
+  //---------------------
 
-    /// Number of script runs
-    var runCount = 0
-    
-    // Prepare Class Loader for compiler
-    //-------------------------
-    var bootclasspath = List[URL]()
-    var bootclasspath2 = List[String]()
+  /// Number of script runs
+  var runCount = 0
 
-    //--- Scala Compiler and library
-    val compilerPath = java.lang.Class.forName("scala.tools.nsc.Interpreter").getProtectionDomain.getCodeSource.getLocation
-    val libPath = java.lang.Class.forName("scala.Some").getProtectionDomain.getCodeSource.getLocation
-    
-    println("Updated code")
-    val runtimeObject = java.lang.Class.forName("scala.runtime.RichInt").getProtectionDomain.getCodeSource.getLocation
-    
-    //-- Mex
-    val mexPath = java.lang.Class.forName("com.idyria.osi.ooxoo.model.ModelCompiler").getProtectionDomain.getCodeSource.getLocation
+  // Prepare Class Loader for compiler
+  //-------------------------
+  var bootclasspath = List[URL]()
+  var bootclasspath2 = List[String]()
 
-    bootclasspath = compilerPath :: libPath :: mexPath :: runtimeObject ::bootclasspath 
+  //--- Scala Compiler and library
+  val compilerPath = java.lang.Class.forName("scala.tools.nsc.Interpreter").getProtectionDomain.getCodeSource.getLocation
+  val libPath = java.lang.Class.forName("scala.Some").getProtectionDomain.getCodeSource.getLocation
 
-    //-- If Classloader is an URL classLoader, add all its urls to the compiler
-    //println("Classloader type: "+getClass().getClassLoader())
-    if (getClass.getClassLoader.isInstanceOf[URLClassLoader]) {
+  println("Updated code")
+  val runtimeObject = java.lang.Class.forName("scala.runtime.RichInt").getProtectionDomain.getCodeSource.getLocation
 
-        //println("Adding URLs from class loader to boot class path")
+  //-- Mex
+  val mexPath = java.lang.Class.forName("com.idyria.osi.ooxoo.model.ModelCompiler").getProtectionDomain.getCodeSource.getLocation
 
-        //-- Gather URLS
-        getClass.getClassLoader.asInstanceOf[URLClassLoader].getURLs().foreach {
-            url => 
-              
-               var urlFile = new File(url.getFile)
-               bootclasspath2 = urlFile.getAbsolutePath :: bootclasspath2
-              	
-               //println(s" -> URL translated: ${urlFile.toURI.toURL}")
-               bootclasspath = url ::  bootclasspath 
-        }
+  bootclasspath = compilerPath :: libPath :: mexPath :: runtimeObject :: bootclasspath
 
+  //-- If Classloader is an URL classLoader, add all its urls to the compiler
+  //println("Classloader type: "+getClass().getClassLoader())
+  if (getClass.getClassLoader.isInstanceOf[URLClassLoader]) {
+
+    //println("Adding URLs from class loader to boot class path")
+
+    //-- Gather URLS
+    getClass.getClassLoader.asInstanceOf[URLClassLoader].getURLs().foreach {
+      url =>
+
+        var urlFile = new File(url.getFile)
+        bootclasspath2 = urlFile.getAbsolutePath :: bootclasspath2
+
+        //println(s" -> URL translated: ${urlFile.toURI.toURL}")
+        bootclasspath = url :: bootclasspath
     }
 
+  }
 
-    // Prepare Compiler Settings Settings
-    //----------------
-    var settings2 = new GenericRunnerSettings({
-        error => println(error)
-    })
-    settings2.usejavacp.value = true
-    settings2.bootclasspath.value = (bootclasspath  mkString java.io.File.pathSeparator)+ ";" + (bootclasspath2  mkString java.io.File.pathSeparator)
-    
+  // Prepare Compiler Settings Settings
+  //----------------
+  var settings2 = new GenericRunnerSettings({
+    error => println(error)
+  })
+  settings2.usejavacp.value = true
+  settings2.bootclasspath.value = (bootclasspath mkString java.io.File.pathSeparator) + ";" + (bootclasspath2 mkString java.io.File.pathSeparator)
 
-    
-    //-- Show some infos
-    //println("compilerPath=" + compilerPath);
-    //println("settings.bootclasspath.value=" + settings2.bootclasspath.value);
+  //-- Show some infos
+  //println("compilerPath=" + compilerPath);
+  //println("settings.bootclasspath.value=" + settings2.bootclasspath.value);
 
-    // Create Compiler
+  // Create Compiler
+  //---------------------
+  var interpreterOutput = new StringWriter
+  val imain = new IMain(settings2, new PrintWriter(interpreterOutput))
+
+  // Compilation result
+
+  /**
+   * Binds a named variable to a value for the model compiler
+   */
+  def bind(name: String, value: Any) = {
+
+    imain.bindValue(name, value)
+
+  }
+
+  /**
+   * @return The Model name
+   */
+  def compile(file: File): ModelInfos = {
+
+    // Get Script input
+    //-----------------------
+    var inputModel = Source.fromFile(file).mkString
+
+    // Code analyses
     //---------------------
-    val imain = new IMain(settings2)
 
-    // Compilation result
+    // use package anylis to determine base model name
+    var (modelName, wrappedModel) = """\s*package\s+(.+)\s*""".r.findFirstMatchIn(inputModel) match {
 
-    /**
-        Binds a named variable to a value for the model compiler
-    */
-    def bind(name:String,value: Any) = {
+      //-- If there is a package info, the code must be compilable, and we have to use the package as name basis for the object
+      case Some(p) => (p.group(1), inputModel)
 
-        imain.bindValue(name,value)
-        
+      //-- No package definition, we can wrap the code and add some imports, and the package name stays empty
+      case None =>
 
-    }
-
-    /**
-        @return The Model name
-    */
-    def compile(file: File) : ModelInfos = {
-
-         // Get Script input
-        //-----------------------
-        var inputModel = Source.fromFile(file).mkString
-
-
-        // Code analyses
-        //---------------------
-        
-        // use package anylis to determine base model name
-        var (modelName,wrappedModel) = """\s*package\s+(.+)\s*""".r.findFirstMatchIn(inputModel) match {
-          
-           //-- If there is a package info, the code must be compilable, and we have to use the package as name basis for the object
-          case Some(p) => (p.group(1),inputModel)
-            
-          // No package definition, we can wrap the code and add some imports, and the package name stays empty
-          case None => 
-            
-            ("",s"""
+        ("", s"""
 import com.idyria.osi.ooxoo.model._
 
 $inputModel
             """)
-          
-          
-        }
-        
-        
-
-        // Determine object name
-        //------------
-        """object\s+([A-Za-z]+)\s+.+""".r.findFirstMatchIn(inputModel) match {
-            case Some(matchRes) => modelName = List(modelName ,matchRes.group(1)).filterNot(_ =="").mkString(".")
-            case None =>
-                throw new RuntimeException(s"Could not determine object name of model: $inputModel")
-        }
-        
-        println("")
-
-        // Compile
-        //--------------
-        imain.compileString(wrappedModel) match {
-
-            // OK -> Return Model
-            case true =>
-                
-                var modelInfos = new ModelInfos(modelName)
-
-                println("Compiled Model: "+modelInfos.name)
-                
-                // Get Annotations
-                imain.bindValue("modelInfos",modelInfos)
-                imain.bindValue("file",file)
-                imain.interpret(s"modelInfos.producers=${modelInfos.name}.producers")
-                imain.interpret(s"${modelInfos.name}.sourceFile = file")
-
-                modelInfos
-
-            case false =>
-                throw new RuntimeException("Could not compile")
-        }
-
 
     }
 
-    /**
-        Compile and produce a file
-    */
-    def produce( file: File , producer: ModelProducer, out: Writer) : Unit = {
-
-        // Compile
-        //-------------------
-        var modelInfos = this.compile(file)
-        this.produce(modelInfos,producer,out)
-        
+    // Determine object name
+    //------------
+    """object\s+([A-Za-z]+)\s+.+""".r.findFirstMatchIn(inputModel) match {
+      case Some(matchRes) => modelName = List(modelName, matchRes.group(1)).filterNot(_ == "").mkString(".")
+      case None =>
+        throw new RuntimeException(s"Could not determine object name of model: $inputModel")
     }
 
-    /**
-        Produce an already compiled file
-    */
-    def produce( modelInfos: ModelInfos , producer: ModelProducer, out: Writer)  : Unit = {
+    println("")
 
-    	//println("Produce compiled model: "+modelInfos.name)
-    	
-    	//imain.bindValue(s"${modelInfos.name}",modelInfos)
-        imain.bindValue("producer",producer)
-        imain.bindValue("writer",out)
-        imain.interpret(s"""${modelInfos.name}.name match { case null => ${modelInfos.name}.name = "${modelInfos.name}"; case _ => ;}""") 
-       /* imain.interpret(s"""
+    // Compile
+    //--------------
+    imain.compileString(wrappedModel) match {
+
+      // OK -> Return Model
+      case true =>
+
+        var modelInfos = new ModelInfos(modelName)
+
+        println("Compiled Model2: " + modelInfos.name)
+
+        //  imain.allDefSymbols
+
+        /* imain.allDefSymbols.foreach {
+                  s => println(s"Available term: "+s)
+                }
+                imain.allDefinedNames.foreach {
+                  n => println(s"Available Name: "+n)
+                }*/
+
+        // Get Annotations
+        //----------
+        imain.bindValue("modelInfos", modelInfos)
+        imain.bindValue("file", file)
+
+        // Run
+        //----------------
+        // Interpret
+        imain.interpret(s"modelInfos.producers=${modelInfos.name}.producers") match {
+          case IR.Error => 
+            
+            println(s"Compilation error: ${interpreterOutput.toString()}")
+            throw new RuntimeException(s"Could not interpret content: ${interpreterOutput.toString()}")
+          case _        =>
+        }
+
+        imain.interpret(s"${modelInfos.name}.sourceFile = file")
+
+        modelInfos
+
+      case false =>
+        throw new RuntimeException(s"Could not compile: ${interpreterOutput.toString()}")
+    }
+
+  }
+
+  /**
+   * Compile and produce a file
+   */
+  def produce(file: File, producer: ModelProducer, out: Writer): Unit = {
+
+    // Compile
+    //-------------------
+    var modelInfos = this.compile(file)
+    this.produce(modelInfos, producer, out)
+
+  }
+
+  /**
+   * Produce an already compiled file
+   */
+  def produce(modelInfos: ModelInfos, producer: ModelProducer, out: Writer): Unit = {
+
+    //println("Produce compiled model: "+modelInfos.name)
+
+    //imain.bindValue(s"${modelInfos.name}",modelInfos)
+    imain.bindValue("producer", producer)
+    imain.bindValue("writer", out)
+    imain.interpret(s"""${modelInfos.name}.name match { case null => ${modelInfos.name}.name = "${modelInfos.name}"; case _ => ;}""")
+    /* imain.interpret(s"""
 println("Model infos "+${modelInfos.name})
 //try {
         if (${modelInfos.name}.name==null) {
@@ -209,8 +216,7 @@ println("Model infos "+${modelInfos.name})
 //    case e : Throwable => 
 //}
         """)*/
-        imain.interpret(s"${modelInfos.name}.produce(producer,writer)")
-    }
+    imain.interpret(s"${modelInfos.name}.produce(producer,writer)")
+  }
 
- 
 }
