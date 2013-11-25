f2579f3e797410a1a6aeca00221484643125e6c0
rleys <richard.leys@ziti.uni-heidelberg.de>
2013-11-25 14:18:10 +0100

    JSON: Improved unit test a bit to cover more cases

--- ooxoo-core/src/test/scala/com.idyria.osi.ooxoo.lib.json/JSONTest.scala
+++ ooxoo-core/src/test/scala/com.idyria.osi.ooxoo.lib.json/JSONTest.scala
@@ -16,13 +16,14 @@ import java.io.PrintStream
 import java.io.CharArrayWriter
 import com.idyria.osi.ooxoo.core.buffers.structural.xattribute
 import com.idyria.osi.tea.logging.TLog
+import com.idyria.osi.ooxoo.core.buffers.structural.VerticalBuffer
 
 @xelement(name = "Test")
 class Test extends ElementBuffer {
 
   @xelement(name = "SimpleElement")
   var simpleElement: XSDStringBuffer = null
-  
+
   @xattribute(name = "attr1")
   var attr1: XSDStringBuffer = null
 
@@ -32,7 +33,20 @@ class Test extends ElementBuffer {
   @xelement(name = "SubTest")
   var subTest: SubTest = null
 
-  override def streamIn(du: DataUnit) = {
+  @xelement(name = "Action")
+  var action: Action = new Action
+
+  @xelement(name = "enumelt")
+  var enumelt = new com.idyria.osi.ooxoo.core.buffers.datatypes.EnumerationBuffer {
+
+    type state = Value
+    val stopped, running, closed = Value
+    def selectstopped: Unit = this select this.stopped
+    def selectrunning: Unit = this select this.running
+    def selectclosed: Unit = this select this.closed
+  }
+
+  /*override def streamIn(du: DataUnit) = {
     super.streamIn(du)
 
     println(s"** Got DU in Test")
@@ -42,7 +56,7 @@ class Test extends ElementBuffer {
       case _ =>
         println(s"** elt: " + du.element.name + ", value: " + du.value)
     }
-  }
+  }*/
 
 }
 
@@ -68,6 +82,22 @@ class SubSubTest extends ElementBuffer {
 @xelement(name = "Path")
 class Path extends com.idyria.osi.ooxoo.core.buffers.datatypes.XSDStringBuffer with com.idyria.osi.ooxoo.core.buffers.structural.ElementBuffer {
 
+} 
+
+@xelement(name = "Action")
+class Action extends com.idyria.osi.ooxoo.core.buffers.datatypes.EnumerationBuffer with com.idyria.osi.ooxoo.core.buffers.structural.ElementBuffer {
+
+  type Action = Value
+  val stop, suspend, resume, join = Value
+  def selectstop: Unit = this select this.stop
+  def selectsuspend: Unit = this select this.suspend
+  def selectresume: Unit = this select this.resume
+  def selectjoin: Unit = this select this.join
+}
+object Action {
+
+  def apply() = new Action
+
 }
 
 class JSONTest extends FunSuite {
@@ -76,6 +106,8 @@ class JSONTest extends FunSuite {
       
       "Test" : {
 		  	"_@attr1" : "AttributeValue",
+		    "Action" : "join",
+		  	"enumelt" : "running",
     		"SimpleElement" : "/local/home/rleys/git/extoll2/tourmalet-tester/www-inputdata/i2c.sscript",
     		"MultipleElement": ["1","2",
     		 "3"],
@@ -122,15 +154,16 @@ class JSONTest extends FunSuite {
     //--------------
     var io = new JsonIO(new StringReader(input2))
     io.streamIn
-    
+
     io = new JsonIO(new StringReader(input))
     io.streamIn
   }
 
   test("Simple Input to object") {
 
-    TLog.setLevel(classOf[JsonIO],TLog.Level.FULL)
-    
+    TLog.setLevel(classOf[JsonIO], TLog.Level.FULL)
+    TLog.setLevel(classOf[VerticalBuffer], TLog.Level.FULL)
+
     // Create
     //------------------
     AnyXList(classOf[SubSubTest])
@@ -148,8 +181,12 @@ class JSONTest extends FunSuite {
     expectResult("/local/home/rleys/git/extoll2/tourmalet-tester/www-inputdata/i2c.sscript")(top.simpleElement.toString)
 
     expectResult("AttributeValue")(top.attr1.toString)
-    
+
     expectResult(3)(top.MultipleElement.size)
+
+    // Enum
+    expectResult(top.enumelt.running)(top.enumelt.selectedValue)
+    expectResult(top.action.join)(top.action.selectedValue)
 
     // Sub
     assert(top.subTest != null)
@@ -176,27 +213,27 @@ class JSONTest extends FunSuite {
     top.MultipleElement += "1"
     top.MultipleElement += "2"
     top.MultipleElement += "3"
-      
+
     top.subTest = new SubTest
-    
-    var subsub = new  SubSubTest
+
+    var subsub = new SubSubTest
     subsub.path = new Path
     subsub.path.data = "/local/home/rleys/git/extoll2/tourmalet-tester/www-inputdata/i2c.sscript"
     top.subTest.content += subsub
-    
-    subsub = new  SubSubTest
+
+    subsub = new SubSubTest
     subsub.path = new Path
     subsub.path.data = "/local/home/rleys/git/extoll2/tourmalet-tester/www-inputdata/i2c.sscript"
     top.subTest.content += subsub
 
     var out = new CharArrayWriter
-    var io = new JsonIO(outputArray =out)
+    var io = new JsonIO(outputArray = out)
     top.appendBuffer(io)
 
     top.streamOut()
-    
-    println(s"Result: "+io.finish)
-    
+
+    println(s"Result: " + io.finish)
+
   }
 
 }
