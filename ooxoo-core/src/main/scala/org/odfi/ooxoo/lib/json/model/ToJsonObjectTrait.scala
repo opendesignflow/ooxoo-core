68c202f31bfbeab8565b161f023d7ceb8aff4ed0
Richard Leys <leys.richard@gmail.com>
2022-07-16 15:57:00 +0200

    Improved toLegacyJsonObject method to accept a passed in value for jsonb config for application specific configs

--- ooxoo-core/src/main/scala/org/odfi/ooxoo/lib/json/model/ToJsonObjectTrait.scala
+++ ooxoo-core/src/main/scala/org/odfi/ooxoo/lib/json/model/ToJsonObjectTrait.scala
@@ -3,6 +3,7 @@ package org.odfi.ooxoo.lib.json.model
 
 
 import java.io.StringReader
+import javax.json.bind.Jsonb
 
 
 trait ToJsonObjectTrait {
@@ -16,7 +17,7 @@ trait ToJsonObjectTrait {
     jakarta.json.Json.createReader(new StringReader(toString)).readObject()
   }
 
-  def toLegacyJsonObject = {
-    javax.json.Json.createReader(new StringReader(toString)).readObject()
+  def toLegacyJsonObject(jsonb : Jsonb = JSONHelper.jsonb) = {
+    javax.json.Json.createReader(new StringReader(jsonb.toJson(this))).readObject()
   }
 }
